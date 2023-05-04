176cd958eea562e4c8425cf4b06bb14529f6a78e
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2023-05-04 20:10:39 +0200

    connection to db

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@ import BreakingBadCards from "./components/BreakingBadCards.vue";
 <template>
   <main>
     <h1>Hero goes here</h1>
-    <BreakingBadCards />
+    <Suspense>
+      <template #default>
+        <BreakingBadCards />
+      </template>
+      <template #fallback>
+        <div>
+          <p>Loading...</p>
+        </div>
+      </template>
+    </Suspense>
   </main>
 </template>
--- src/components/BreakingBadCards.vue
+++ src/components/BreakingBadCards.vue
@@ -1,7 +1,63 @@
 <script setup>
+import axios from "axios";
+
+// there is no pagination in this api, to see go to leson number 73
+const response = await axios.get(
+  `https://thronesapi.com/api/v2/Characters?pageSize=10`
+);
+const characters = response.data;
+console.log(characters);
 </script>
 <template>
-  <div>
-    <h1>Breaking Bad Cards3</h1>
+  <div class="container">
+    <div class="cards"></div>
   </div>
 </template>
+<style scoped>
+.container {
+  background-color: rgb(27, 26, 26);
+  padding: 30px;
+}
+
+.cards {
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  height: 700px;
+}
+
+.cards h3 {
+  font-weight: bold;
+}
+
+.cards p {
+  font-size: 10px;
+}
+
+.jobs {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  padding-top: 30px;
+}
+
+.button-container button {
+  border: none;
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  margin: 0 5px;
+  cursor: pointer;
+}
+
+.spinner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
